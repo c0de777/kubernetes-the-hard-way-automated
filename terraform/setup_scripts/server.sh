608d5b03c4ea059f9b19148c67c88cec4d83a922
c0de777 <mvasquez@take2it.com>
2025-12-04 13:57:39 -0600

    Update server.sh
    removed key append

--- terraform/setup_scripts/server.sh
+++ terraform/setup_scripts/server.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#placeholder for your public key variable
-PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9om4JXfgFhjUagycURjkLGWYy8Pk2aHSB8F1qBCQslZR+aFhU2Pqnmhf3FpImOrkAnGjCflRl5yKk7YtRAxdQ/VHx7ROCFWRdYymHU8Q+OjZHcXgCLhPjRxNS8YM9ViK7DXQSZKNBdtmK68hZBr/GhnOa38AkqXgvda+ar9O1cLf8A42Z1XpY+q8wxK0ubjB9Ag8vzJRG+aOpwJ2dUOSG4YRO8zkz+ymcMl6mmr+DThFfoEjn6xcIyPllF6y5boboQ7fHGZqdYD2jjE9iTz4utpp4+22juuoyRKNfr6k9smM+pvQlCD4oXrOPVsnIGDxDof81ytZzoXccutGNGpEu9eG1ctbiqCL22Ll6MIHQ8w08ZRfY3EnJ4JvaMp8wR8LpgAjErLvDivaqbQtyD1uyn9S3lzx41GmnHdGZYdujCIE29fcLLnhBSbE4XsFCujTgHngrFBM2J1IsQLTNyX8MAJoPzFcE3gEhhz/FFcQSOn0KRWk/yTqCE+52aoqT1sM="
-
-# Append public key to known_hosts
-echo "$PUBLIC_KEY" >> /home/ec2-user/.ssh/known_hosts
-
 # Set hostname
 hostnamectl set-hostname server.kubernetes.local
 systemctl restart systemd-hostnamed
