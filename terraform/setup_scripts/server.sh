189b59a8be475863864cc23f0a05da8e85989200
c0de777 <mvasquez@take2it.com>
2025-12-05 13:14:52 -0600

    Update server.sh

--- terraform/setup_scripts/server.sh
+++ terraform/setup_scripts/server.sh
@@ -7,8 +7,6 @@ systemctl restart systemd-hostnamed
 
 # Permit root login in sshd_config (optional)
 sed -i 's/^#*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-# Restart sshd to apply changes
 systemctl restart sshd
 
 # Wait for jumpbox to finish
@@ -26,10 +24,48 @@ sudo mkdir -p /etc/etcd /var/lib/etcd
 sudo chmod 700 /var/lib/etcd
 sudo cp /home/ubuntu/ca.crt /home/ubuntu/kube-api-server.key /home/ubuntu/kube-api-server.crt /etc/etcd/
 
-# --- Install the systemd unit file ---
+# --- Install the etcd systemd unit file ---
 sudo mv /home/ubuntu/etcd.service /etc/systemd/system/
 
 # --- Start the etcd server ---
 sudo systemctl daemon-reload
 sudo systemctl enable etcd
 sudo systemctl start etcd
+
+# --- Create the Kubernetes configuration directory ---
+sudo mkdir -p /etc/kubernetes/config
+
+# --- Install the Kubernetes controller binaries ---
+sudo mv /home/ubuntu/kube-apiserver /usr/local/bin/
+sudo mv /home/ubuntu/kube-controller-manager /usr/local/bin/
+sudo mv /home/ubuntu/kube-scheduler /usr/local/bin/
+sudo mv /home/ubuntu/kubectl /usr/local/bin/
+
+# --- Configure the Kubernetes API server ---
+sudo mkdir -p /var/lib/kubernetes/
+sudo mv /home/ubuntu/ca.crt /home/ubuntu/ca.key \
+  /home/ubuntu/kube-api-server.key /home/ubuntu/kube-api-server.crt \
+  /home/ubuntu/service-accounts.key /home/ubuntu/service-accounts.crt \
+  /home/ubuntu/encryption-config.yaml \
+  /var/lib/kubernetes/
+
+# --- Install the kube-apiserver systemd unit file ---
+sudo mv /home/ubuntu/kube-apiserver.service /etc/systemd/system/kube-apiserver.service
+
+# --- Configure the Kubernetes Controller Manager ---
+sudo mv /home/ubuntu/kube-controller-manager.kubeconfig /var/lib/kubernetes/
+sudo mv /home/ubuntu/kube-controller-manager.service /etc/systemd/system/
+
+# --- Configure the Kubernetes Scheduler ---
+sudo mv /home/ubuntu/kube-scheduler.kubeconfig /var/lib/kubernetes/
+sudo mv /home/ubuntu/kube-scheduler.yaml /etc/kubernetes/config/
+sudo mv /home/ubuntu/kube-scheduler.service /etc/systemd/system/
+
+# --- Start the controller services ---
+sudo systemctl daemon-reload
+sudo systemctl enable kube-apiserver kube-controller-manager kube-scheduler
+sudo systemctl start kube-apiserver kube-controller-manager kube-scheduler
+
+# --- Apply RBAC config for API server to talk to kubelet ---
+kubectl apply -f /home/ubuntu/kube-apiserver-to-kubelet.yaml \
+  --kubeconfig /home/ubuntu/admin.kubeconfig
