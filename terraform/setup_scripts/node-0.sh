c599298ddb9f553368921f7083ba517598adebd5
c0de777 <mvasquez@take2it.com>
2025-12-05 12:50:37 -0600

    Update node-0.sh
    added wait for jumpbox.done

--- terraform/setup_scripts/node-0.sh
+++ terraform/setup_scripts/node-0.sh
@@ -10,3 +10,20 @@ sed -i 's/^#*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
 # Restart sshd to apply changes
 systemctl restart sshdstance_type = "t3.micro"
 
+# Wait for jumpbox to finish
+while [ ! -f /home/ubuntu/jumpbox.done ]; do
+  echo "Waiting for jumpbox to finish cert distribution..."
+  sleep 30
+done
+
+# --- REST NEEDED ---
+
+
+
+
+
+
+
+# Continue with Kubernetes startup
+sudo systemctl start kubelet
+sudo systemctl start kube-proxy
